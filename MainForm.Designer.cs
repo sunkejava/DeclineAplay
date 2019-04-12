bef5a90ae283d7f0b22d95866f8d1fc4d905cce5
sunkejava_WorkPc <1577972070@qq.com>
2019-04-12 14:53:38 +0800

    添加项目文件。

---
+++ MainForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace DeclineAplay
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Panel_main = new LayeredSkin.Controls.LayeredPanel();
+            this.SuspendLayout();
+            // 
+            // Panel_main
+            // 
+            this.Panel_main.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Panel_main.Borders.BottomColor = System.Drawing.Color.Empty;
+            this.Panel_main.Borders.BottomWidth = 1;
+            this.Panel_main.Borders.LeftColor = System.Drawing.Color.Empty;
+            this.Panel_main.Borders.LeftWidth = 1;
+            this.Panel_main.Borders.RightColor = System.Drawing.Color.Empty;
+            this.Panel_main.Borders.RightWidth = 1;
+            this.Panel_main.Borders.TopColor = System.Drawing.Color.Empty;
+            this.Panel_main.Borders.TopWidth = 1;
+            this.Panel_main.Canvas = ((System.Drawing.Bitmap)(resources.GetObject("Panel_main.Canvas")));
+            this.Panel_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Panel_main.Location = new System.Drawing.Point(0, 0);
+            this.Panel_main.Name = "Panel_main";
+            this.Panel_main.Size = new System.Drawing.Size(728, 487);
+            this.Panel_main.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(728, 487);
+            this.Controls.Add(this.Panel_main);
+            this.EnabledDWM = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.IsLayeredWindowForm = false;
+            this.Name = "MainForm";
+            this.Radius = 25;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LayeredSkin.Controls.LayeredPanel Panel_main;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LayeredSkin.Forms;
+using LayeredSkin.Controls;
+using LayeredSkin.DirectUI;
+
+namespace DeclineAplay
+{
+    public partial class MainForm : LayeredForm
+    {
+        private AxAPlayer3Lib.AxPlayer axPlayer1;
+        delegate void AsynUpdateConfigInfoUI(DuiTextBox dtb);//委托更新加载控件显示
+        LayeredPanel lp_main = new LayeredPanel();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            axPlayer1 = new AxAPlayer3Lib.AxPlayer();
+            axPlayer1.Size = new Size(this.Width - 10, this.Height - 10);
+            axPlayer1.Location = new Point(5, 5);
+            this.Panel_main.Controls.Add(axPlayer1);
+            axPlayer1.Visible = true;
+            Bitmap bmp = new Bitmap(Image.FromFile(@"E:\CNetProject\DeclineAplay\WindowsFormsApplication1\Resources\2.png"));
+            axPlayer1.SetCustomLogo(bmp.GetHbitmap().ToInt32());
+            axPlayer1.SetConfig(207, "1");//智能去除当前视频黑边
+            axPlayer1.SetConfig(36, "16761024;50;20");//Logo 风格设置，格式："backcolor;xpercent;ypercent"，例如设置白色背景(16777215为0xffffff的十进制串)、水平偏左(30%)、垂直居中(50%)的 Logo 位置："16777215;30;50"
+
+            axPlayer1.SetVolume(5);
+            axPlayer1.Open("http://hd.yinyuetai.com/uploads/videos/common/E6E90165F112591DC08AF52DA40112E9.mp4?sc=dfeae283fd371dfd&br=1094&vid=3293228&aid=39611&area=KR&vst=0");
+            axPlayer1.Play();
+
+            lp_main.Size = new Size(100, 300);
+            lp_main.BackColor = Color.Transparent;
+            Panel_main.Controls.Add(lp_main);
+            lp_main.Location = new Point(0, 0);
+
+            DuiTextBox lbt = new DuiTextBox();
+            lbt.Size = lp_main.Size;
+            lbt.Multiline = true;
+            lp_main.DUIControls.Add(lbt);
+            lbt.Location = new Point(0, 0);
+            lp_main.BringToFront();
+            lbt.BackColor = Color.Transparent;
+            //getAplayerConfigInfo(lbt);
+            lbt.Text = axPlayer1.GetConfig(205);
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            axPlayer1.Dispose();
+            this.Close();
+        }
+
+        private void getAplayerConfigInfo(DuiTextBox lbt)
+        {
+            if (lp_main.InvokeRequired)
+            {
+                AsynUpdateConfigInfoUI au = new AsynUpdateConfigInfoUI(getAplayerConfigInfo);
+                this.Invoke(au, new object[] { lbt });
+            }
+            {
+                for (int i = 1; i < 42; i++)
+                {
+                    lbt.Text += "Aplayer配置" + i.ToString() + "内容为：" + axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 101; i < 122; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 201; i < 222; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 301; i < 316; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 401; i < 418; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 501; i < 512; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 601; i < 625; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 701; i < 713; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 801; i < 805; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 901; i < 908; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 1001; i < 1005; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 1101; i < 1112; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 1301; i < 1329; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 1401; i < 1416; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 1501; i < 1504; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 1801; i < 1805; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 2101; i < 2112; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 2201; i < 2208; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 2301; i < 2314; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 2401; i < 2411; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 2501; i < 2519; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+                for (int i = 2601; i < 2610; i++)
+                {
+                    lbt.Text += axPlayer1.GetConfig(i) + "\r\n";
+                }
+            }
+        }
+    }
+}
