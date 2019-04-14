54749b29361e8860cfc16b32f5f79a64685fb273
sunkejava <declineaberdeen@foxmail.com>
2019-04-14 13:34:38 +0800

    Aplayer属性事件完善

--- AplayControl/AplayerBase.cs
+++ AplayControl/AplayerBase.cs
@@ -7,37 +7,171 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using AxAPlayer3Lib;
+using LayeredSkin.Controls;
 
 namespace DeclineAplay.AplayControl
 {
-    public partial class AplayerBase : Panel
+    public partial class AplayerBase : LayeredBaseControl
     {
         public AxPlayer _axPlayer;
+        private Image defaultImage;
+        private Color aplaybackColor;
+        private bool speedup = false;
+        private Point logoLocation;
+
         public AplayerBase()
         {
             InitializeComponent();
             _axPlayer = new AxPlayer();
         }
-
-        [Category("Aplayer自带方法"), Description("设置APlayer引擎视频区域在未播放视频时显示的图片"), Browsable(true)]
-        public bool setDefaultLogo(Image img)
+        #region Aplayer自带属性
+        [Description("设置APlayer引擎视频区域在未播放视频时显示的图片"), Category("Aplayer自带属性")]
+        public Image DefaultImage {
+            get
+            {
+                return defaultImage;
+            }
+            set
+            {
+                defaultImage = value;
+            }
+        }
+        [Description("设置APlayer引擎视频区域在未播放视频时的背景色"), Category("Aplayer自带属性")]
+        public Color AplayBackColor
         {
-            Bitmap bmp = new Bitmap(img);
-            _axPlayer.SetCustomLogo(bmp.GetHbitmap().ToInt32());
-            return true;
+            get
+            {
+                return aplaybackColor;
+            }
+            set
+            {
+                aplaybackColor = value;
+            } 
         }
 
-        [Category("Aplaye"), Description("设置cs"), Browsable(true)]
-        public bool isSetW(bool isa)
+        [Description("获取或设置是否开启硬件加速，1-开启，0-不开启"), Category("Aplayer自带属性")]
+        public Boolean Speedup
         {
-
-            return isa;
+            get
+            {
+                return speedup;
+            }
+            set
+            {
+                speedup = value;
+            }
         }
+        [Description("Logo 风格设置设置logo位置，配合AplayBackColor使用，有一个未设置均不生效"), Category("Aplayer自带属性")]
+        public Point LogoLocation
+        {
+            get
+            {
+                return logoLocation;
+            }
+            set
+            {
+                logoLocation = value;
+            }
+        }
+        #endregion
+
+        #region Aplayer自带方法
+        [Description("打开需要播放的媒体文件"), Category("Aplayer自带方法")]
+        public void Open(string url)
+        {
+            _axPlayer.Open(url);
+        }
+        [Description("使 APlayer 引擎由暂停状态进入播放状态"), Category("Aplayer自带方法")]
+        public void Play()
+        {
+            _axPlayer.Play();
+        }
+        [Description("使 APlayer 引擎由播放状态进入暂停状态"), Category("Aplayer自带方法")]
+        public void Pause()
+        {
+            _axPlayer.Pause();
+        }
+        [Description("关闭 APlayer 引擎正在播放的媒体文件"), Category("Aplayer自带方法")]
+        public void Close()
+        {
+            _axPlayer.Close();
+        }
+        [Description("设置 APlayer 引擎"), Category("Aplayer自带方法")]
+        public int SetConfig(int nConfigId, string strValue)
+        {
+            return _axPlayer.SetConfig(nConfigId, strValue);
+        }
+        [Description("获取 APlayer 引擎的设置"), Category("Aplayer自带方法")]
+        public string GetConfig(int nConfigId)
+        {
+            return _axPlayer.GetConfig(nConfigId);
+        }
+        [Description("获取 APlayer 引擎的版本号"), Category("Aplayer自带方法")]
+        public string GetVersion()
+        {
+            return _axPlayer.GetVersion();
+        }
+        [Description("获取 APlayer 引擎的当前状态"), Category("Aplayer自带方法")]
+        public int GetState()
+        {
+            return _axPlayer.GetState();
+        }
+        [Description("获取当前播放媒体文件的持续时长"), Category("Aplayer自带方法")]
+        public int GetDuration()
+        {
+            return _axPlayer.GetDuration();
+        }
+        [Description("获取当前播放媒体文件的播放进度"), Category("Aplayer自带方法")]
+        public int GetPosition()
+        {
+            return _axPlayer.GetPosition();
+        }
+        [Description("设置当前播放媒体文件的播放位置"), Category("Aplayer自带方法")]
+        public int SetPosition(int nPosition)
+        {
+            return _axPlayer.SetPosition(nPosition);
+        }
+        [Description("获取当前播放音量"), Category("Aplayer自带方法")]
+        public int GetVolume()
+        {
+            return _axPlayer.GetVolume();
+        }
+        [Description("设置当前播放音量"), Category("Aplayer自带方法")]
+        public int SetVolume(int nVolume)
+        {
+            return _axPlayer.SetVolume(nVolume);
+        }
+        [Description("获取 APlayer 引擎当前是否处于设置播放进度(Seek)过程中"), Category("Aplayer自带方法")]
+        public int IsSeeking()
+        {
+            return _axPlayer.IsSeeking();
+        }
+        [Description("获取 APlayer 引擎播放网络文件时的数据缓冲进度"), Category("Aplayer自带方法")]
+        public int GetBufferProgress()
+        {
+            return _axPlayer.GetBufferProgress();
+        }
+        #endregion
 
         protected override void OnPaint(PaintEventArgs pe)
         {
-            _axPlayer.Dock = DockStyle.Fill;
             this.Controls.Add(_axPlayer);
+            _axPlayer.Dock = DockStyle.Fill;
+            if (defaultImage is null)
+            {
+                _axPlayer.SetCustomLogo(-1);
+            }
+            else
+            {
+                //设置背景图片
+                Bitmap bmp = new Bitmap(defaultImage);
+                _axPlayer.SetCustomLogo(bmp.GetHbitmap().ToInt32());
+            }
+            //设置背景颜色及自定义logo位置aplaybackColor.ToArgb().ToString() +
+            _axPlayer.SetConfig(36,  "16777215;" + (logoLocation.X).ToString() + ";" + (logoLocation.Y).ToString());
+            //是否硬件加速
+            _axPlayer.SetConfig(209, speedup ? "1" : "0");
+
             base.OnPaint(pe);
         }
     }
--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.Panel_main = new LayeredSkin.Controls.LayeredPanel();
             this.aplayerBase1 = new DeclineAplay.AplayControl.AplayerBase();
+            this.btn_play = new LayeredSkin.Controls.LayeredButton();
             this.Panel_main.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,26 +47,74 @@
             this.Panel_main.Borders.TopColor = System.Drawing.Color.Empty;
             this.Panel_main.Borders.TopWidth = 1;
             this.Panel_main.Canvas = ((System.Drawing.Bitmap)(resources.GetObject("Panel_main.Canvas")));
+            this.Panel_main.Controls.Add(this.btn_play);
             this.Panel_main.Controls.Add(this.aplayerBase1);
             this.Panel_main.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Panel_main.Location = new System.Drawing.Point(0, 0);
             this.Panel_main.Name = "Panel_main";
-            this.Panel_main.Size = new System.Drawing.Size(728, 487);
+            this.Panel_main.Size = new System.Drawing.Size(728, 527);
             this.Panel_main.TabIndex = 0;
             // 
             // aplayerBase1
             // 
-            this.aplayerBase1.Location = new System.Drawing.Point(315, 89);
+            this.aplayerBase1.AplayBackColor = System.Drawing.Color.White;
+            this.aplayerBase1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.aplayerBase1.Borders.BottomColor = System.Drawing.Color.Empty;
+            this.aplayerBase1.Borders.BottomWidth = 1;
+            this.aplayerBase1.Borders.LeftColor = System.Drawing.Color.Empty;
+            this.aplayerBase1.Borders.LeftWidth = 1;
+            this.aplayerBase1.Borders.RightColor = System.Drawing.Color.Empty;
+            this.aplayerBase1.Borders.RightWidth = 1;
+            this.aplayerBase1.Borders.TopColor = System.Drawing.Color.Empty;
+            this.aplayerBase1.Borders.TopWidth = 1;
+            this.aplayerBase1.Canvas = ((System.Drawing.Bitmap)(resources.GetObject("aplayerBase1.Canvas")));
+            this.aplayerBase1.DefaultImage = global::DeclineAplay.Properties.Resources._19;
+            this.aplayerBase1.Location = new System.Drawing.Point(3, 20);
+            this.aplayerBase1.LogoLocation = new System.Drawing.Point(50, 50);
             this.aplayerBase1.Name = "aplayerBase1";
-            this.aplayerBase1.Size = new System.Drawing.Size(306, 282);
+            this.aplayerBase1.Size = new System.Drawing.Size(722, 455);
+            this.aplayerBase1.Speedup = true;
             this.aplayerBase1.TabIndex = 0;
+            // 
+            // btn_play
+            // 
+            this.btn_play.AdaptImage = true;
+            this.btn_play.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_play.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_play.Borders.BottomColor = System.Drawing.Color.Empty;
+            this.btn_play.Borders.BottomWidth = 1;
+            this.btn_play.Borders.LeftColor = System.Drawing.Color.Empty;
+            this.btn_play.Borders.LeftWidth = 1;
+            this.btn_play.Borders.RightColor = System.Drawing.Color.Empty;
+            this.btn_play.Borders.RightWidth = 1;
+            this.btn_play.Borders.TopColor = System.Drawing.Color.Empty;
+            this.btn_play.Borders.TopWidth = 1;
+            this.btn_play.Canvas = ((System.Drawing.Bitmap)(resources.GetObject("btn_play.Canvas")));
+            this.btn_play.ControlState = LayeredSkin.Controls.ControlStates.Normal;
+            this.btn_play.HaloColor = System.Drawing.Color.White;
+            this.btn_play.HaloSize = 5;
+            this.btn_play.HoverImage = null;
+            this.btn_play.IsPureColor = false;
+            this.btn_play.Location = new System.Drawing.Point(333, 485);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.NormalImage = null;
+            this.btn_play.PressedImage = null;
+            this.btn_play.Radius = 15;
+            this.btn_play.ShowBorder = true;
+            this.btn_play.Size = new System.Drawing.Size(76, 30);
+            this.btn_play.TabIndex = 1;
+            this.btn_play.Text = "播放";
+            this.btn_play.TextLocationOffset = new System.Drawing.Point(0, 0);
+            this.btn_play.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            this.btn_play.TextShowMode = LayeredSkin.TextShowModes.Halo;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(728, 487);
+            this.ClientSize = new System.Drawing.Size(728, 527);
             this.Controls.Add(this.Panel_main);
             this.EnabledDWM = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
@@ -75,7 +124,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
-            this.Load += new System.EventHandler(this.MainForm_Load);
             this.Panel_main.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -85,6 +133,7 @@
 
         private LayeredSkin.Controls.LayeredPanel Panel_main;
         private AplayControl.AplayerBase aplayerBase1;
+        private LayeredSkin.Controls.LayeredButton btn_play;
     }
 }
 
